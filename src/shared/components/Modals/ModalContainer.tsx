b978aa10e5122f86ff779dd1d5284947e531278b
Waren Gador <waren.gador15@gmail.com>
2026-07-15 17:19:50 +0800

    update

--- src/shared/components/Modals/ModalContainer.tsx
+++ src/shared/components/Modals/ModalContainer.tsx
@@ -1,19 +1,16 @@
 import { Form } from "@dev-waren/react-form-kit";
-import type { ChangeEventHandler } from "react";
-import type { FieldValues, UseFormReturn } from "react-hook-form";
+import type { ComponentProps } from "react";
 
 type ModalContainerProps = {
-  form: {
-    methods: UseFormReturn<FieldValues>;
-    onSubmit: ChangeEventHandler<HTMLFormElement, Element>;
-    children: React.ReactNode;
-  };
+  form: ComponentProps<typeof Form>;
 };
 
 export function ModalContainer({ form }: ModalContainerProps) {
   return (
-    <div className="p-4 rounded-md shadow-md">
-      <Form {...form}></Form>
+    <div className="fixed inset-0 bg-black/70">
+      <div className="mx-auto mt-20 h-[12em] w-1/2 rounded-md p-4 shadow-md">
+        <Form {...form} />
+      </div>
     </div>
   );
 }
