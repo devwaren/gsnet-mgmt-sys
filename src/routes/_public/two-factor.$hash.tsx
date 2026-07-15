6516869803a4d1c65c59d0f4336b160edecc8d49
Waren Gador <waren.gador15@gmail.com>
2026-07-15 16:50:10 +0800

    update

--- src/routes/_public/two-factor.$hash.tsx
+++ src/routes/_public/two-factor.$hash.tsx
@@ -1,15 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { TwoWayVerificationForm } from "#/features/auth/two-way";
+import { TwoWayVerificationForm } from "#/features/auth/two-factor";
 import { AuthLayout } from "#/shared/layouts";
 
 export const Route = createFileRoute("/_public/two-factor/$hash")({
-	component: RouteComponent,
+  component: RouteComponent,
 });
 
 function RouteComponent() {
-	return (
-		<AuthLayout>
-			<TwoWayVerificationForm />
-		</AuthLayout>
-	);
+  return (
+    <AuthLayout>
+      <TwoWayVerificationForm />
+    </AuthLayout>
+  );
 }
