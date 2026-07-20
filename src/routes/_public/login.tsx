617fc5dbeda5db3c73853cfb25958cc4db11ef0c
Waren Gador <waren.gador15@gmail.com>
2026-07-20 08:43:20 +0800

    update

--- src/routes/_public/login.tsx
+++ src/routes/_public/login.tsx
@@ -1,15 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
+
 import { LoginForm } from "#/features/auth/login";
 import { AuthLayout } from "#/shared/layouts";
 
 export const Route = createFileRoute("/_public/login")({
-	component: RouteComponent,
+  component: RouteComponent,
 });
 
 function RouteComponent() {
-	return (
-		<AuthLayout>
-			<LoginForm />
-		</AuthLayout>
-	);
+  return (
+    <AuthLayout>
+      <LoginForm />
+    </AuthLayout>
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { LoginForm } from "../features/auth/login";
+import { AuthLayout } from "../shared/layouts";
+
 export const Route = createFileRoute("/")({
-	component: Home,
+  component: Home,
 });
 
 function Home() {
-	return (
-		<div className="p-8">
-			<h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-			<p className="mt-4 text-lg">
-				Edit <code>src/routes/index.tsx</code> to get started.
-			</p>
-		</div>
-	);
+  return (
+    <AuthLayout>
+      <LoginForm />
+    </AuthLayout>
+  );
 }
